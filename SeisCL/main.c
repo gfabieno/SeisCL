ac6ec0469fe6467709b22099fd08884fdbcb46b0
GFO <Gabriel.Fabien-Ouellet@ete.inrs.ca>
2016-12-13 12:00:04 -0800

    Initial Commit

---
+++ SeisCL/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SeisCL
+//
+//  Created by Gabriel Fabien-Ouellet on 16-12-13.
+//  Copyright © 2016 Gabriel Fabien-Ouellet. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
