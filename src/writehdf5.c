9d23d07c12522c36ffc21d47cfe055b0244b9bff
GFO <Gabriel.Fabien-Ouellet@ete.inrs.ca>
2017-08-17 14:18:04 -0700

    Chunk experiment

--- src/writehdf5.c
+++ src/writehdf5.c
@@ -47,6 +47,8 @@ void writetomat(hid_t* file_id,
         for (ii=0;ii<NDIMs;ii++){
             cdims[ii]=cdims[ii]<dims[ii]?cdims[ii]:dims[ii];
         }
+        cdims[0]=dims[0];
+        cdims[1]=1;
         H5Pset_chunk (plist_id, NDIMs, cdims);
 //        H5Pset_deflate (plist_id, 6);
         
