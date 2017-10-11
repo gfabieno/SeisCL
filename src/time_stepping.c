2ab1c246af930eaeaa35ec6aff1304d1b1d89b2a
Gabriel Fabien-Ouellet <gfabieno@hotmail.com>
2017-10-11 10:48:31 -0700

    debug

--- src/time_stepping.c
+++ src/time_stepping.c
@@ -390,13 +390,13 @@ int initialize_grid(model * m, device ** dev, int s){
         
         // Implent initial conditions
         __GUARD prog_launch( &(*dev)[d].queue, &(*dev)[d].bnd_cnds.init_f);
-//        __GUARD prog_launch( &(*dev)[d].queue, &(*dev)[d].src_recs.varsoutinit);
-//
-//
-//        if (m->GRADOUT==1 && m->BACK_PROP_TYPE==2){
-//            __GUARD prog_launch( &(*dev)[d].queue,
-//                                 &(*dev)[d].grads.initsavefreqs);
-//        }
+        __GUARD prog_launch( &(*dev)[d].queue, &(*dev)[d].src_recs.varsoutinit);
+
+
+        if (m->GRADOUT==1 && m->BACK_PROP_TYPE==2){
+            __GUARD prog_launch( &(*dev)[d].queue,
+                                 &(*dev)[d].grads.initsavefreqs);
+        }
         
 
         
