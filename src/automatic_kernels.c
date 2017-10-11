c0005391a7546f912b1bb1de2ca08f67a5d1a256
Gabriel Fabien-Ouellet <gfabieno@hotmail.com>
2017-10-11 10:50:22 -0700

    debug

--- src/automatic_kernels.c
+++ src/automatic_kernels.c
@@ -255,7 +255,7 @@ int kernel_varinit(device * dev,
     strcat(temp, "\n}");
     
     
-    printf("%s\n\n%lu\n",temp, strlen(temp));
+//    printf("%s\n\n%lu\n",temp, strlen(temp));
     
     
     (*prog).src=temp;
