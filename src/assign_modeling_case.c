1328dc5da15c6adfe757b804e11613e9e66e6714
Gabriel Fabien-Ouellet <gfabieno@hotmail.com>
2017-10-17 17:51:13 -0700

    debug

--- src/assign_modeling_case.c
+++ src/assign_modeling_case.c
@@ -869,7 +869,7 @@ int set_scalers( void *mptr){
         }
     }
 
-    scaler = -log2(Mmax*m->dt/m->dh*100);
+    scaler = -log2(Mmax*m->dt/m->dh*1);
 //    scaler = 0;
     
     variable * var;
