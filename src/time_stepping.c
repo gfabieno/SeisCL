1afb348193b44ea3e39dbcda6345d91d442d4239
Gabriel Fabien-Ouellet <gfabieno@hotmail.com>
2017-11-10 09:48:32 -0800

    debug

--- src/time_stepping.c
+++ src/time_stepping.c
@@ -504,12 +504,12 @@ int time_stepping(model * m, device ** dev) {
 
 
             // Outputting seismograms
-            if (m->VARSOUT>0 || m->GRADOUT || m->RMSOUT || m->RESOUT){
-                for (d=0;d<m->NUM_DEVICES;d++){
-                    __GUARD prog_launch( &(*dev)[d].queue,
-                                        &(*dev)[d].src_recs.varsout);
-                }
-            }
+//            if (m->VARSOUT>0 || m->GRADOUT || m->RMSOUT || m->RESOUT){
+//                for (d=0;d<m->NUM_DEVICES;d++){
+//                    __GUARD prog_launch( &(*dev)[d].queue,
+//                                        &(*dev)[d].src_recs.varsout);
+//                }
+//            }
 
             // Outputting the movie
             if (m->MOVOUT>0 && (t+1)%m->MOVOUT==0 && state==0)
