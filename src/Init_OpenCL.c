9d90794273b6e5e381d40749ad0ae250370e0a8e
Gabriel Fabien-Ouellet <gfabieno@hotmail.com>
2017-11-13 13:58:07 -0800

    debug

--- src/Init_OpenCL.c
+++ src/Init_OpenCL.c
@@ -982,7 +982,7 @@ int Init_CUDA(model * m, device ** dev)  {
         
     }
     
-    fprintf(stdout,"%d %d \n",m->NUM_DEVICES, m->NLOCALP)è
+    fprintf(stdout,"%d %d \n",m->NUM_DEVICES, m->NLOCALP);
     
 
     if (state && m->MPI_INIT==1)
