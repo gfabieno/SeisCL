a12078614a6ab59a1463ba9591f50cb580718fdc
Gabriel Fabien-Ouellet <gfabieno@hotmail.com>
2017-11-16 10:31:35 -0800

    debug

--- src/Init_OpenCL.c
+++ src/Init_OpenCL.c
@@ -772,7 +772,7 @@ int Init_CUDA(model * m, device ** dev)  {
             GMALLOC(di->vars_adj, sizeof(variable)*m->nvars);
             for (i=0;i<m->nvars;i++){
                 di->vars_adj[i]=di->vars[i];
-                di->vars_adj[i].cl_varout.free_host=1;
+                di->vars_adj[i].cl_varout.free_host=0;
             }
             
             for (i=0;i<m->nvars;i++){
