940c96b1a6d6f6ed36824b1cfa3cec4ec93acf57
Gabriel <gabriel.fabien-ouellet@polymtl.ca>
2019-10-10 16:05:43 -0400

    added setup.py

---
+++ SeisCL/__init__.py
@@ -0,0 +1,1 @@
+from .SeisCL import *
---
+++ setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='SeisCL',
+      version='1.0',
+      description='Interface to SeisCL, for seismic modeling and inversion',
+      long_description=readme(),
+      author='Gabriel Fabien-Ouellet',
+      author_email='gabriel.fabien-ouellet@polymtl.ca',
+      license='GNU General Public License v3.0',
+      packages=['SeisCL'],
+      install_requires=['obspy',
+                        'numpy',
+                        'hdf5storage',
+                        'h5py',
+                        'scipy'],
+      zip_safe=False)
